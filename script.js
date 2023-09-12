930f07f8cdef7221c40f0645c1031c9ec1c331d4
Adrian-YongKeng <141707001+Adrian-YongKeng@users.noreply.github.com>
2023-09-12 07:13:38 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+function startQuiz() {
+  const guess = prompt('What is What does this ✅ stands for?');
+  if (guess.toLowerCase() === 'tick') {
+    alert('You guessed corretly !');}
+  else {
+    alert('Sorry, the correct answer is Tick !');
+  }
+
+
+  const guess2 = prompt('What is What does this 🧡 stands for?');
+  if (guess2.toLowerCase() === 'love') {
+    alert('You guessed corretly !');}
+  else {
+    alert('Sorry, the correct answer is Love !');
+  }
+
+
+  const guess3 = prompt('What is What does this 🍔 stands for?');
+  if (guess3.toLowerCase() === 'burger') {
+    alert('You guessed corretly !'); }
+  else {
+    alert('Sorry, the correct answer is Burger !');
+  }
+
+
+  const guess4 = prompt('What is What does this 🍉 stands for?');
+  if (guess4.toLowerCase() === 'watermelon') {
+    alert('You guessed corretly !');}
+  else {
+    alert('Sorry, the correct answer is Watermelon !');
+  }
+  
+}
